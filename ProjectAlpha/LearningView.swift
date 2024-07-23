0308c6b846c7c501809b3a73b210e9551df8a2e9
justCod1ng <iutjourney@gmail.com>
2024-07-24 04:29:23 +0500

    LearningView

---
+++ ProjectAlpha/LearningView.swift
@@ -0,0 +1,23 @@
+//
+//  LearningView.swift
+//  ProjectAlpha
+//
+//  Created by justCod1ng on 24/07/24.
+//
+
+import SwiftUI
+
+struct LearningView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                
+            }
+            .navigationTitle("Learning")
+        }
+    }
+}
+
+#Preview {
+    LearningView()
+}
