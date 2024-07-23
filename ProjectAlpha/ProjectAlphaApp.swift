b9f1549cf61bb420a54b411ae1995ddba70d4f18
Davron Abdukhakimov <davron0506150@gmail.com>
2024-07-23 14:51:23 +0500

    Initial Commit

---
+++ ProjectAlpha/ProjectAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAlphaApp.swift
+//  ProjectAlpha
+//
+//  Created by Davron Abdukhakimov on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
